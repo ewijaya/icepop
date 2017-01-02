dcfa95776e4ab489676fdf83da1eeeee72eee554
ewijaya <ewijaya@ifrec.osaka-u.ac.jp>
2017-01-02 15:06:18 +0900

    Add circos configuration files.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import warnings
 
 setup(name='icepop',
       #version.release.build
-      version='1.4.2',
+      version='1.4.3',
       author='Edward Wijaya',
       description='Python package for analysing immune cell population in expressed genes',
       license='MIT License',
